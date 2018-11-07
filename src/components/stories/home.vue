01500d13deb19cc3570778d3302fed187689bbc8
Abhishek Jha <j7.abhi@gmail.com>
2018-11-07 22:39:27 +0530

    story component created

---
+++ src/components/stories/home.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="upper">
+        <!-- Written summary of the art by the author -->
+        <story :content="bigLorem" :title="lorem_title" :creator="lorem_artist"></story> 
+    </div>
+    <div class="lower">
+        <!-- Navigational component that will take the user to  videos, pictures and everything else about the piece -->
+    </div>
+    </div>
+</template>
+
+<script>
+import Story from './story.vue'
+
+export default {
+    components:{
+        'story' : Story
+    },
+    computed:{
+      bigLorem : function(){
+            return (this.lorem+this.lorem+this.lorem+this.lorem+this.lorem+this.lorem+this.lorem)
+      }  
+    },
+    data(){
+        return {
+            lorem : 'Lorem ipsum dolor sit amet vrhejvrehvjer vervre bvrbtbr sfdffe verggerg dgtbgtebg rvretcscfsfddffrg ghrrdwdwe nmnb  hbjh kbhjhbhjvrnkjbk kjbjrhevbje kbkhewb consectetur adipisicing elit. Labore cum, ut vel dolor unde non! Odit possimus eaque natus asperiores sapiente blanditiis repellat ad? Debitis quo ipsum dicta voluptas necessitatibus?',
+            lorem_artist:'Abhishek Jha',
+            lorem_title: 'A Beautiful Title',
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .upper{
+        max-height: 100px!important;
+        width: 100vw;
+    }
+</style>
---
+++ src/components/stories/story.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h2 class="heading">
+            {{title}}
+        </h2>
+        <h5 class="heading thin right" >
+            {{creator}}
+        </h5>
+        </div>
+        <div class="row">
+            <div class="flow-text">
+                {{content}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Gallery from './../landing/gallery.vue'
+export default {
+    components:{
+        'gallery' : Gallery
+    },
+    props:{
+        content: {
+            type: String
+        },
+        title:{
+
+        },
+        images: {
+            type: Array
+        },
+        creator: {
+            type: Array
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
